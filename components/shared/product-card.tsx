97521ed2cac4b35a3bca054e1910df45742bfa2f
Vitaliy <bering856gmail.com>
2024-12-27 18:49:09 +0200

    feat: add dashboard and layout components, restructure product page

--- components/shared/product-card.tsx
+++ components/shared/product-card.tsx
@@ -23,10 +23,12 @@ export const ProductCard: React.FC<Props> = ({ id, name, price, count, imageUrl,
         <div className="flex justify-center p-6 bg-secondary rounded-lg h-[260px]">
           <Image width={215} height={215} src={imageUrl} alt={name} style={{ width: "auto", height: "auto" }} />
         </div>
-      </Link>
-      <Title text={name} size="sm" className="mb-1 mt-3 font-bold" />
-      <p className="text-sm text-gray-400">Chicken, mozzarella, cheddar and parmesan cheeses, cheese sauce, tomatoes, Alfredo sauce, garlic.</p>
 
+        <Title text={name} size="sm" className="mb-1 mt-3 font-bold" />
+        <p className="text-sm text-gray-400">
+          Chicken, mozzarella, cheddar and parmesan cheeses, cheese sauce, tomatoes, Alfredo sauce, garlic.
+        </p>
+      </Link>
       <div className="flex justify-between items-center mt-4">
         <span className="text-[20px]">
           from <b>${price}</b>
