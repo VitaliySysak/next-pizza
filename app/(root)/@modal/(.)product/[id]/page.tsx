dde1b5d1e418509a401f716ea97251ad4e8f93ac
Vitaliy <bering856gmail.com>
2024-12-29 11:53:11 +0200

    refactor: simplify ProductModalPage function and add "use client" directive in choose-pizza-form

--- app/(root)/@modal/(.)product/[id]/page.tsx
+++ app/(root)/@modal/(.)product/[id]/page.tsx
@@ -2,11 +2,7 @@ import { ChooseProductModal } from "@/components/shared";
 import { prisma } from "@/prisma/prisma-client";
 import { notFound } from "next/navigation";
 
-async function ProductModalPage({
-  params: { id },
-}: {
-  params: { id: string };
-}) {
+async function ProductModalPage({ params: { id } }: { params: { id: string } }) {
   const product = await prisma.product.findFirst({
     where: {
       id: Number(id),
--- components/shared/modals/choose-product-modal.tsx
+++ components/shared/modals/choose-product-modal.tsx
@@ -14,6 +14,7 @@ interface Props {
 
 export const ChooseProductModal: React.FC<Props> = ({ product, className }) => {
   const router = useRouter();
+  
   return (
     <Dialog open={Boolean(product)} onOpenChange={() => router.back()}>
       <DialogContent className="p-0 w-[1060px] max-w-[1060px] min-h-[500px] bg-white overflow-hidden">
