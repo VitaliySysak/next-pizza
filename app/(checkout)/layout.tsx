d4bdfd4f056947fde2460d66db602cf312238709
Vitaliy <bering856@gmail.com>
2025-03-12 20:56:45 +0200

    fix

--- app/(checkout)/layout.tsx
+++ app/(checkout)/layout.tsx
@@ -1,5 +1,6 @@
 import { Container, Header } from "@/src/components/shared";
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Next Pizza | Cart",
@@ -10,8 +11,8 @@ export default function CheckoutLayout({ children }: { children: React.ReactNode
   return (
     <main className="min-h-screen bg-[#F4F1EE]">
       <Container>
-      <Header hasSearch={false} hasCart={false} className="border-gray-200" />
-      {children}
+        <Header hasSearch={false} hasCart={false} className="border-gray-200" />
+        <Suspense>{children}</Suspense>
       </Container>
     </main>
   );
