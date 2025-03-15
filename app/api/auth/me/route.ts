a0c261930a12203d1b94847e468af67d4eccbfaa
Vitaliy <bering856@gmail.com>
2025-03-15 13:56:50 +0200

    fix

--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import { prisma } from "@/prisma/prisma-client";
 import { getUserSession } from "@/src/lib/get-user-session";
 import { NextResponse } from "next/server";
