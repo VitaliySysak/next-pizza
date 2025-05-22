937b2218c5363fa285e217323ef9584a4ef5ba11
Vitaliy <bering856@gmail.com>
2025-05-22 18:24:37 +0300

    fix email

--- src/lib/send-email.ts
+++ src/lib/send-email.ts
@@ -1,13 +1,13 @@
-import { Resend } from 'resend';
+import { Resend } from "resend";
 
 export const sendEmail = async (to: string, subject: string, template: React.ReactNode) => {
   const resend = new Resend(process.env.RESEND_API_KEY);
 
   const { data, error } = await resend.emails.send({
-    from: 'onboarding@resend.dev',
+    from: process.env.NEXT_PUBLIC_EMAIL_FROM!,
     to,
     subject,
-    text: '',
+    text: "",
     react: template,
   });
 
